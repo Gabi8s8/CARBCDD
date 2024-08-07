6bf1e503d879c420351973deb99d1e3e812b2a82
Gabi8s8 <gabriela.teixeira1@aluno.ifsp.edu.br>
2024-08-07 10:40:10 -0300

    Aula 07-08

---
+++ 2024-07-31/hello-prisma/insert-user.js
@@ -0,0 +1,11 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.user.create({
+        data: {
+            name: 'Hello, Prisma!',
+        }
+    })
+}
